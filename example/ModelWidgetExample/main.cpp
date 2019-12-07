40301e335561a4806c5a15c63e468add8bb921bf
Nir Jacobson <nirjacobson@gmail.com>
2019-12-06 23:25:40 -0500

    model widget example: ensure 32-bit color in surface format

--- example/ModelWidgetExample/main.cpp
+++ example/ModelWidgetExample/main.cpp
@@ -1,9 +1,17 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QSurfaceFormat>
 
 int main(int argc, char *argv[])
 {
+    QSurfaceFormat format;
+    format.setRedBufferSize(8);
+    format.setGreenBufferSize(8);
+    format.setBlueBufferSize(8);
+    format.setAlphaBufferSize(8);
+    QSurfaceFormat::setDefaultFormat(format);
+
     QApplication a(argc, argv);
     a.setStyle("fusion");
     Sahara::ModelWidgetExample::MainWindow w;
